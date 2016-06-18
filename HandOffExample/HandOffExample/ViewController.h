71a627ad411f0a1c0e748a65473302767902f2f2
devcampbell <vegas.lover.gp@gmail.com>
2016-06-17 17:57:59 -0700

    Added HandOffExample project

---
+++ HandOffExample/HandOffExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HandOffExample
+//
+//  Created by Gaurav Pal on 6/17/16.
+//  Copyright © 2016 help. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
